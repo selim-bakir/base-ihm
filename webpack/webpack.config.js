26ce66de9cf5c87baa29bfa45605f788135a7367
Ulrick92 <37837145+Ulrick92@users.noreply.github.com>
2020-12-01 12:07:51 +0100

    slider-tabs-mobile

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -38,7 +38,7 @@ let config = {
     devtool: dev ? 'cheap-eval-source-map' : false,
     devServer: {
         contentBase: path.resolve(__dirname, 'src'),
-        host: '192.168.0.21',
+        host: 'localhost',
         port: '9000',
         disableHostCheck: false,
         open: true,
